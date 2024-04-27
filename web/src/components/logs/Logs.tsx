1dc798c0d383d30f65590c98abd6bf01cfd512ea
Elizar Pepino <jupenz@gmail.com>
2024-04-28 01:18:05 +0800

    updates some styles

--- web/src/components/logs/Logs.tsx
+++ web/src/components/logs/Logs.tsx
@@ -12,9 +12,24 @@ export interface LogsProp {
 
 export type LogSummaries = Array<LogSummary>;
 
+function Log(props: LogSummary) {
+    return <div class="flex code border-b border-r border-l border-black cursor-pointer hover:bg-blue-100">
+        <div class="p-3 flex-1">{(new Date(props.date)).toLocaleDateString()}</div>
+        <div class="p-3 flex-1">{props.server_name}</div>
+        <div class="p-3 flex-1">{props.winner}</div>
+        <div class="p-3 flex-1">{props.mvp}</div>
+    </div>;
+};
+
 export default function Logs(props: LogsProp) {
     const logs = () => props.logs;
-    return <>
-        {logs().map(l => <p>{(new Date(l.date)).toLocaleDateString()} | {l.server_name} | {l.winner} | {l.mvp}</p>)}
-    </>;
+    return <div class="border-b-4 border-r-4 border-black">
+        <div class="bg-black flex text-white text-sm border border-black">
+            <div class="pl-3 pt-1 pb-1 pr-3 flex-1">DATE</div>
+            <div class="pl-3 pt-1 pb-1 pr-3 flex-1">SERVER</div>
+            <div class="pl-3 pt-1 pb-1 pr-3 flex-1">WINNER</div>
+            <div class="pl-3 pt-1 pb-1 pr-3 flex-1">MVP</div>
+        </div>
+        {logs().map(l => <Log {...l} />)}
+    </div>;
 };
