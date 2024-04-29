a68c1f1e471fdef3a3e370579998941d7980560b
Elizar Pepino <jupenz@gmail.com>
2024-04-29 08:51:58 +0800

    ui goodness!

--- web/src/Header.tsx
+++ web/src/Header.tsx
@@ -1,14 +1,16 @@
+import { A } from "@solidjs/router"
+
 export default function Header() {
     return <header class="top-0 left-0 w-full z-10 flex items-center justify-center">
         <div class="flex justify-center w-10/12 lg:w-1/2 pb-5 pt-5">
             <nav class="flex-1">
-                <a href="/" class="font-black text-4xl text-black">gsparser</a>
+                <A href="/" class="font-black text-4xl text-black">gsparser</A>
             </nav>
             <div class="flex-1 flex items-center justify-end">
-                <a href="/submit" class="p-1 font-medium pl-4 pr-4 border-2 bg-blue-300 border-black hover:shadow-[4px_4px_0px_rgba(0,0,0,1)] transition duration-200">
-                    SUBMIT LOG
-                </a>
+                <A href="/submit" class="p-1 font-medium pl-4 pr-4 border-2 bg-blue-300 border-black hover:shadow-[4px_4px_0px_rgba(0,0,0,1)] transition duration-200">
+                    Upload log
+                </A>
             </div>
         </div>
-    </header>
+    </header >
 };
--- web/src/components/logs/Logs.tsx
+++ web/src/components/logs/Logs.tsx
@@ -1,3 +1,4 @@
+import { A } from "@solidjs/router";
 import Card from "../Card";
 
 export interface LogSummary {
@@ -14,24 +15,58 @@ export interface LogsProp {
 
 export type LogSummaries = Array<LogSummary>;
 
+const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+
 function Log(props: LogSummary) {
-    return <div class="transition duration-300 flex code border-b border-r border-l border-black cursor-pointer hover:bg-gray-200">
-        <div class="p-3 flex-1">{(new Date(props.date)).toLocaleDateString()}</div>
-        <div class="p-3 flex-1">{props.server_name}</div>
-        <div class="p-3 flex-1">{props.winner}</div>
-        <div class="p-3 flex-1">{props.mvp}</div>
-    </div>;
+    const date = new Date(props.date);
+    return <Card class="code border-2 mb-4 shadow-blue-300 hover:bg-yellow-100 hover:shadow-black transition duration-200">
+        <A href="/">
+            <span class="p-1 pl-3 bg-yellow-300 flex text-black text-sm border-b-2 border-black uppercase tracking-widest">
+                {props.server_name}
+            </span>
+            <span class="flex">
+                <span class="border-r border-black p-3 flex-col flex items-center justify-center">
+                    <p class="text-center font-bold text-2xl">{date.getDate()}</p>
+                    <p class="text-center text-sm text-black">{months[date.getMonth()]}</p>
+                </span>
+                <span class="flex-grow block">
+                    <span class="flex items-center p-3 font-bold flex-1 border-b border-black">
+                        <span class="inline-block w-8 h-8 mr-3">
+                            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                width="30px" height="30px" viewBox="0 0 128 128" enable-background="new 0 0 128 128">
+                                <path fill="#000000" d="M112,36c-6.629,0-12,5.375-12,12c0,1.68,0.352,3.273,0.973,4.727L84,60L69.801,34.445
+	C73.48,32.391,76,28.508,76,24c0-6.625-5.371-12-12-12s-12,5.375-12,12c0,4.508,2.52,8.391,6.199,10.445L44,60l-16.973-7.273
+	C27.648,51.273,28,49.68,28,48c0-6.625-5.371-12-12-12S4,41.375,4,48s5.371,12,12,12c0.93,0,1.822-0.133,2.695-0.328L28,100v8
+	c0,4.422,3.582,8,8,8h56c4.418,0,8-3.578,8-8v-8l9.309-40.328C110.176,59.875,111.07,60,112,60c6.629,0,12-5.375,12-12
+	S118.629,36,112,36z M64,20c2.207,0,4,1.797,4,4s-1.793,4-4,4s-4-1.797-4-4S61.793,20,64,20z M12,48c0-2.203,1.793-4,4-4
+	s4,1.797,4,4s-1.793,4-4,4S12,50.203,12,48z M92,108H36v-8h56V108z M93.633,92H34.367L27.34,61.563l13.508,5.789
+	C41.871,67.789,42.941,68,43.996,68c2.828,0,5.547-1.5,6.996-4.117L64,40.477l13.008,23.406C78.457,66.5,81.176,68,84.004,68
+	c1.055,0,2.125-0.211,3.148-0.648l13.508-5.789L93.633,92z M112,52c-2.207,0-4-1.797-4-4s1.793-4,4-4s4,1.797,4,4S114.207,52,112,52
+	z"/>
+                            </svg>
+                        </span>
+                        {props.winner}
+                    </span>
+                    <span class="flex items-center p-3 font-bold flex-1">
+                        <span class="inline-block w-8 h-8 mr-3">
+                            <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+                                <g id="SVGRepo_iconCarrier"> <path d="M12 15C15.7279 15 18.75 12.0899 18.75 8.5C18.75 4.91015 15.7279 2 12 2C8.27208 2 5.25 4.91015 5.25 8.5C5.25 12.0899 8.27208 15 12 15Z" stroke="000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /> <path d="M7.51999 13.52L7.51001 20.9C7.51001 21.8 8.14001 22.24 8.92001 21.87L11.6 20.6C11.82 20.49 12.19 20.49 12.41 20.6L15.1 21.87C15.87 22.23 16.51 21.8 16.51 20.9V13.34" stroke="000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /> </g>
+
+                            </svg>
+                        </span>
+                        {props.mvp}
+                    </span>
+                </span>
+            </span>
+        </A>
+    </Card>;
 };
 
 export default function Logs(props: LogsProp) {
     const logs = () => props.logs;
-    return <Card>
-        <div class="bg-black flex text-white text-sm border border-black code">
-            <div class="pl-3 pt-1 pb-1 pr-3 flex-1">DATE</div>
-            <div class="pl-3 pt-1 pb-1 pr-3 flex-1">SERVER</div>
-            <div class="pl-3 pt-1 pb-1 pr-3 flex-1">WINNER</div>
-            <div class="pl-3 pt-1 pb-1 pr-3 flex-1">MVP</div>
-        </div>
+    return <>
         {logs().map(l => <Log {...l} />)}
-    </Card>;
+    </>;
 };
